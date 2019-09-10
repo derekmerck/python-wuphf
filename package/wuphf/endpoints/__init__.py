e92b6abd7478a0972deb54ab917e2669d66a2fba
Derek Merck <derek.merck@ufl.edu>
2019-09-10 11:03:12 -0400

    make study workflow use observable objects

--- package/wuphf/endpoints/__init__.py
+++ package/wuphf/endpoints/__init__.py
@@ -1,4 +1,4 @@
 from .slack_messenger import SlackMessenger
 from .smtp_messenger import SmtpMessenger
-from .twilio_messenger import TwilioMessenger
+# from .twilio_messenger import TwilioMessenger
 from .email_sms_messenger import EmailSMSMessenger
